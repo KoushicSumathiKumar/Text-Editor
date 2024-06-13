3fdbc54f3bba9caa7b7a1e15550f885650e99a12
Koushic <koushic.sk@yahoo.com>
2024-06-13 14:57:10 +0100

    Added Speech to Text

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 from tkinter.filedialog import askopenfilename, asksaveasfilename
 from tkinter import messagebox as mb
+import speech_recognition as sr
 
 
 def open_file(window, text):
@@ -19,7 +20,7 @@ def open_file(window, text):
     window.title(filepath)
 
     # Notification to alert the user
-    tk.messagebox.showinfo("Notification","File Opened") 
+    mb.showinfo("Notification", "File Opened") 
 
 
 def save_file(window, text):
@@ -37,11 +38,12 @@ def save_file(window, text):
     window.title(filepath)
 
     # Notification to alert the user
-    tk.messagebox.showinfo("Notification", "File Saved") 
+    mb.showinfo("Notification", "File Saved") 
+
 
 def new_file(window, text):
-    save_or_no=mb.askquestion("Notification", "Would you like to save the current file?")
-    if save_or_no == "yes" :
+    save_or_no = mb.askquestion("Notification", "Would you like to save the current file?")
+    if save_or_no == "yes":
         save_file(window, text)
 
     # "1.0" -> "1" = 1st line and "0" means 1st column
@@ -50,17 +52,39 @@ def new_file(window, text):
     window.title("New File")
 
     # Notification to alert the user
-    tk.messagebox.showinfo("Notification","New File Opened")
+    mb.showinfo("Notification", "New File Opened")
+
 
 def new_window():
     main()
     # Notification to alert the user
-    tk.messagebox.showinfo("Notification","New Window Opened") 
+    mb.showinfo("Notification", "New Window Opened") 
+
 
 def shortcuts():
     # Notification containing all of the shortcuts
-    tk.messagebox.showinfo("Notification", "Ctrl + N -> New File \nCtrl + O -> Open File \nCtrl + W -> Open New Window \nCtrl + S -> Save File") 
+    mb.showinfo("Notification", "Ctrl + N -> New File\nCtrl + O -> Open File\nCtrl + W -> Open New Window\nCtrl + S -> Save File") 
 
+
+def speech_to_text(window, text):
+    recogniser = sr.Recognizer()
+    with sr.Microphone() as source:
+        # Notification to alert the user
+        mb.showinfo("Notification", "Speak now after closing the notification... With a pause you can stop the Speech to Text")
+        audio = recogniser.listen(source)
+
+        try:
+            mb.showinfo("Notification", "Translating your Speech to text")
+            recognised_text = recogniser.recognize_google(audio)
+            text.insert(tk.END, recognised_text)
+
+        except sr.UnknownValueError:
+            # Error message if the Speech Regonition couldn't understand the users speech/audio
+            mb.showerror("Error", "Speech Recognition could not understand audio")
+            return
+        except sr.RequestError as e:
+            # Error message if the request isn't handled by the server
+            mb.showerror("Error", f"Could not request results from Speech Recognition service; {e}")
 
 
 def main():
@@ -74,10 +98,6 @@ def main():
     text_editor.grid(row=0, column=1)
     text_editor.pack(expand="yes", fill="both")
 
-
-    # relief=tk.RAISED is to give 3D Effect
-    # frame = tk.Frame(window, relief=tk.RAISED,bd=2)
-
     # File-menu
     menu_bar = tk.Menu(window)
     window.config(menu=menu_bar)
@@ -88,7 +108,6 @@ def main():
     menu_bar.add_cascade(label="File", menu=file_menu)
     menu_bar.add_cascade(label="Help", menu=help_menu)
 
-
     file_menu.add_command(label="New", command=lambda: new_file(window, text_editor))
     file_menu.add_command(label="Open", command=lambda: open_file(window, text_editor))
     file_menu.add_command(label="Open New Window", command=lambda: new_window())
@@ -96,17 +115,14 @@ def main():
     file_menu.add_separator()
     file_menu.add_command(label="Exit", command=window.destroy)
 
-
     help_menu.add_command(label="Keyboard Shortcuts", command=lambda: shortcuts())
-
+    help_menu.add_command(label="Speech to Text", command=lambda: speech_to_text(window, text_editor))
 
     # Key binds for shortcuts
     window.bind("<Control-o>", lambda x: open_file(window, text_editor))
     window.bind("<Control-s>", lambda x: save_file(window, text_editor))
     window.bind("<Control-n>", lambda x: new_file(window, text_editor))
     window.bind("<Control-w>", lambda x: new_window())
-
-
 
     window.mainloop()
 
